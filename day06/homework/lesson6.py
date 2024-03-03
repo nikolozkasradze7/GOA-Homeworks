e9da4bb49aeacdfb103d87787f5f830c096edde3
nikolozkasradze7 <nkasradze0@gmail.com>
2024-03-03 20:38:41 +0400

    commit

---
+++ day06/homework/lesson6.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter your first number:"))
+num2 = int(input("enter your second number:"))
+num3 = int(input("enter your third number:"))
+
+print(int(num1) + int(num2) + int(num3))        
+print(int(num1) / int(num2) / int(num3))  
+print(int(num1) * int(num2) * int(num3)) 
+print(int(num1) - int(num2) - int(num3))
+                                               
---
+++ day07/classwork/lesson.py
@@ -0,0 +1,12 @@
+print(int(5,8))
+print(int("8")  + 6)
+
+print(float("2,7"))
+print(float(8))
+
+print(str(8) + "4")
+print(str(2,1) + "6")
+
+print(type(2,8))
+print(type(7))
+print(type("nikolozi"))
